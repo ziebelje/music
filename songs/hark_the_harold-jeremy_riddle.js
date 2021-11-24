13652a025de6a018865405a3323198003d8baa37
Jon Ziebell <ziebelje@gmail.com>
2021-11-23 22:08:01 -0500

    Formatting

--- songs/hark_the_harold-jeremy_riddle.js
+++ songs/hark_the_harold-jeremy_riddle.js
@@ -7,30 +7,50 @@ ccp.songs.push({
   'example': undefined,
   'chart': `[Verse 1]
 E
-Hark the herald angels sing "Glory to the new born King
-E                              B7
-Peace on earth and mercy mild  God and sinners reconciled"
-E             A       B      E                A      B
-Joyful all ye nations rise   Join the triumph of the skies
-A            F#m              B         E       A   B E
-With angelic host proclaim  "Christ is born in Bethlehem"
-A               F#m           B     E      F#m  B    E
-Hark the herald angels sing "Glory to the new born King"
+Hark the herald angels sing
+
+"Glory to the new born King
+E
+Peace on earth and mercy mild
+B7
+God and sinners reconciled"
+E             A       B
+Joyful all ye nations rise
+E                A      B
+Join the triumph of the skies
+A            F#m
+With angelic host proclaim
+B          E       A    B  E
+"Christ is born in Beth-le-hem"
+A               F#m
+Hark the herald angels sing
+B      E      F#m  B    E
+"Glory to the new  born King"
 
 [Instrumental]
 E  A  C#m  A
 
 [Verse 2]
 E
-Christ, by highest heaven adored; Christ the everlasting Lord;
-E                             B7
-Late in time behold him come, Offspring of the Virgin's womb.
-E/G#                 A        B    E                  A  B
-Veiled in flesh, the God-head see; hail the incarnate Diety
-A                   F#m            B      E     A B E
-Pleased as man with men to dwell, Jesus, our Immanuel
-A               F#m           B     E      F#m  B    E
-Hark the herald angels sing "Glory to the new born King"
+Christ, by highest heaven adored;
+
+Christ the everlasting Lord;
+E
+Late in time behold him come,
+B7
+Offspring of the Virgin's womb.
+E/G#                 A        B
+Veiled in flesh, the God-head see;
+E                  A  B
+hail the incarnate Diety
+A                   F#m
+Pleased as man with men to dwell,
+B      E      A   B   E
+Jesus, our Im-man-u - el
+A               F#m
+Hark the herald angels sing
+B      E      F#m  B    E
+"Glory to the new  born King"
 
 [Instrumental]
 E  A  C#m  A
@@ -38,15 +58,25 @@ E  A  C#m  A
 
 [Verse 3]
 E
-Hail! the heaven-born Prince of Peace. Hail the son of Righteousness
-E                                B7
-Light and life to all He brings, risen with healing in His wings
-E/G#             A     B   E/G#             A        B
-Mild He lays His glory by, born that man no more may die
-A                 F#m             B       E         A B    E
-Born to raise the sons of earth, born to give them second birth
-A               F#m           B     E      F#m  B    E
-Hark the herald angels sing "Glory to the new born King"
+Hail! the heaven-born Prince of Peace.
+
+Hail the son of Righteousness
+E
+Light and life to all He brings,
+B7
+risen with healing in His wings
+E/G#             A     B
+Mild He lays His glory by,
+E/G#             A        B
+born that man no more may die
+A                 F#m
+Born to raise the sons of earth,
+B       E         A  B    E
+born to give them se-cond birth
+A               F#m
+Hark the herald angels sing
+B      E      F#m  B    E
+"Glory to the new  born King"
 
 [Instrumental]
 E  A  C#m  A
@@ -54,15 +84,25 @@ E  A  C#m  A
 
 [Verse 3]
 E
-Hail! the heaven-born Prince of Peace. Hail the son of Righteousness
-E                                B7
-Light and life to all He brings, risen with healing in His wings
-E/G#             A     B   E/G#             A        B
-Mild He lays His glory by, born that man no more may die
-A                 F#m             B       E         A B    E
-Born to raise the sons of earth, born to give them second birth
-A               F#m           B     E      F#m  B    E
-Hark the herald angels sing "Glory to the new born King"
+Hail! the heaven-born Prince of Peace.
+
+Hail the son of Righteousness
+E
+Light and life to all He brings,
+B7
+risen with healing in His wings
+E/G#             A     B
+Mild He lays His glory by,
+E/G#             A        B
+born that man no more may die
+A                 F#m
+Born to raise the sons of earth,
+B       E         A  B    E
+born to give them se-cond birth
+A               F#m
+Hark the herald angels sing
+B      E      F#m  B    E
+"Glory to the new  born King"
 
 [Instrumental]
 E  A  C#m  A
