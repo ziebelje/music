20d301d7b945eed326df816363648a900551b764
Jon Ziebell <ziebelje@gmail.com>
2021-12-22 06:12:26 -0500

    Updates

---
+++ songs/big_block_chevrolet-zackary_dean_and_the_midnight_orchestra.js
@@ -0,0 +1,141 @@
+ccp.songs.push({
+  'title': 'Big Block Chevrolet',
+  'author': 'Zackary Dean & The Midnight Orchestra',
+  'key': 'G',
+  'tempo': 78,
+  'timing': '4/4',
+  'example': undefined,
+  'chart': `[Intro Riff]
+e|-----------------------|
+B|-----------------------|
+G|-2h4p2p0---------------|
+D|---------0h2p0---------|
+A|---------------0h2p0---|
+E|---------------------3-|
+
+[Intro]
+G
+
+[Verse 1]
+     G                       D/F#
+She' old, but she aint tired
+          Em
+Yeah they put her all together
+                       C
+in the summer of sixty-nine
+         G                   D/F#
+She don' leak and she sounds mean
+      Em
+Yeah, hummin down the road just
+                     C
+lookin and breathing fire
+
+[Chorus]
+C                G
+Shes a big block Chevrolet
+D/F#                       Em
+Rolling down the road just haulin' hey
+C                        G
+Aint you the girl that I used to know
+D/F#                     Em
+Ball cap denim with your high heels on
+
+C         G
+Big Block Chevrolet
+D/F#                   Em
+Callin you up pick you up at 8
+C            G
+33 inches of mud and lift
+D/F#                           Em
+Slinging down the road like my daddy did
+
+C             D                   G
+Hey, just two kids In a big block Chevrolet
+
+[Verse 2]
+G                  D/F#
+I grew up. Went to school
+Em
+Got married had a baby,
+                         C
+had another and divorced too
+          G               D/F#
+What aint changed My best friend
+     Em
+Is a big red pickem up,
+D
+layin rubber, blowin dust,
+C
+454 again
+
+[Chorus]
+C                G
+Shes a big block Chevrolet
+D/F#                       Em
+Rolling down the road just haulin' hey
+C                        G
+Aint you the girl that I used to know
+D/F#                     Em
+Ball cap denim with your high heels on
+
+C         G
+Big Block Chevrolet
+D/F#                   Em
+Callin you up pick you up at 8
+C            G
+33 inches of mud and lift
+D/F#                           Em
+Slinging down the road like my daddy did
+
+C             D                   G
+Hey, just two kids In a big block Chevrolet
+
+
+
+[Solo]
+C G
+C G
+C G D
+
+// Repeat 2x
+
+G
+
+[Bridge]
+G                       D/F#
+And that girl I used to know
+Em                                 C
+Ball cap denim with the high heels on
+        G             D/F#
+Took my hand, took my heart
+
+Em
+Stole my keys and my truck
+D
+Reved it high, picked me up
+C
+hit the mud and we got stuck
+
+[Chorus]
+C                G
+Shes a big block Chevrolet
+D/F#                       Em
+Rolling down the road just haulin' hey
+C                        G
+Aint you the girl that I used to know
+D/F#                     Em
+Ball cap denim with your high heels on
+
+C         G
+Big Block Chevrolet
+D/F#                   Em
+Callin you up pick you up at 8
+C            G
+33 inches of mud and lift
+D/F#                           Em
+Slinging down the road like my daddy did
+
+C             D                   G
+Hey, just two kids In a big block Chevrolet
+`
+});
---
+++ songs/gone-elevation_worship.js
@@ -0,0 +1,119 @@
+ccp.songs.push({
+  'title': 'Gone',
+  'author': 'Elevation Worship',
+  'key': 'G',
+  'tempo': 82,
+  'timing': '4/4',
+  'example': 'https://youtu.be/3_xSniDTf7o?t=608',
+  'chart': `[Intro]
+G
+Whoah, whoah
+G
+Whoah, whoah
+G
+Whoah, whoah
+Em     C     G
+Whoah, whoah
+
+[Verse 1]
+   G                           C         G
+It wasn't for nothing that You shed Your blood
+   D                      Em       Cadd9
+So I'm gonna live like my shame is gone
+G                        C     G
+Won't be shackled to the way I was
+    D                      C
+Oh, I'm gonna live like my chains are
+
+[Chorus]
+Em    D/F#
+Gone, gone
+G      C                Em
+Now my sin is dead and gone
+           D       G
+And I sing hallelujah
+Em    D/F#
+Done, done
+G     C             Em
+He is risen, it is done
+           D          G
+And I sing hallelujah
+
+[Turnaround]
+G
+Whoah, whoah
+
+[Verse 2]
+G                            C     G
+Praise is a weapon that will over--come
+    D                        Em       Cadd9
+Oh, I'm gonna shout like the battle's won
+Em        D                 C       G
+Fall back devil 'cause Your time is up
+    D                       C
+Oh, I'm gonna live like the stone is
+
+[Chorus]
+Em    D/F#
+Gone, gone
+G      C                Em
+Now my sin is dead and gone
+           D       G
+And I sing hallelujah
+Em    D/F#
+Done, done
+G     C             Em
+He is risen, it is done
+           D          G
+And I sing hallelujah
+
+[Bridge]
+
+How great the power of the blood (Oh, oh)
+  G      C                G
+I am the righteousness of God
+Em  C     G
+Oh,       oh
+    Em        D/F#         G   C
+How great the power of the blood
+Em  D  G
+Oh,    oh
+  Em     D/F#              G  C
+I am the righteousness of God
+Em  D  G
+Oh,    oh
+Em  D  G
+Oh,    oh
+
+[Chorus]
+Em    D/F#
+Gone, gone
+G      C                Em
+Now my sin is dead and gone
+           D       G
+And I sing hallelujah
+Em    D/F#
+Done, done
+G     C             Em
+He is risen, it is done
+           D          G
+And I sing hallelujah
+
+// Repeat 2x
+
+[Outro]
+G
+Whoah, whoah
+G
+Whoah, whoah
+G
+Whoah, whoah
+G
+Whoah, whoah
+
+// I believe the IV chord is always an add9
+// on the recording but I don't hear that in
+//the MR version. Reduced it to just once in
+// each verse.
+`
+});
