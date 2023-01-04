af915296f76ba6d9a77d74411729e90dec0aadc6
Jon Ziebell <ziebelje@gmail.com>
2023-01-03 22:57:41 -0500

    Songs

---
+++ songs/lord_i_need_you-passion.js
@@ -0,0 +1,79 @@
+ccp.songs.push({
+  'title': 'Lord, I Need You',
+  'author': 'Passion',
+  'key': 'C',
+  'tempo': 74,
+  'timing': '4/4',
+  'example': 'https://youtu.be/I2jkIHAeCWo?t=3941',
+  'chart': `[Intro]
+C  Fadd9  C
+
+[Verse 1]
+        C     Fadd9    C
+Lord, I come, I     confess
+G/B    Am7    G       Fadd9
+Bowing here I find my rest
+        C     Fadd9  C
+Without You I fall  apart
+           Gsus G                Fadd9
+You're the One    that guides my heart
+
+[Chorus]
+        C         Fadd9  C    G
+Lord, I need You, oh,  I need You
+Am7   Fadd9  C    G
+Every hour I need You
+   C     Fadd9     C        Fadd9
+My one defense, my righteousness
+   C          G    C
+Oh God, how I need You
+
+[Verse 2]
+               C         Fadd9    C
+Where sin runs deep Your grace is more
+               Am7      G         Fadd9
+Where grace is found is where You are
+          C          Fadd9   C
+Where You are, Lord, I    am free
+    Gsus G            C
+Holiness   is Christ in me
+          C          Fadd9   C
+Where You are, Lord, I    am free
+    Gsus G            C
+Holiness   is Christ in me
+
+[Chorus]
+        C         Fadd9  C    G
+Lord, I need You, oh,  I need You
+Am7   Fadd9  C    G
+Every hour I need You
+   C     Fadd9     C        Fadd9
+My one defense, my righteousness
+   C          G    C
+Oh God, how I need You
+
+[Bridge]
+   Fadd9    C/E     G/B     Am7
+So teach my song to rise to You
+Fadd9           G        Fadd9
+When temptation comes my way
+       Fadd9  C/E        G/B     Am7
+When I cannot stand I'll fall on You
+Fadd9            G        C
+Jesus, You're my hope and stay
+       Fadd9  C/E        G/B     Am7
+When I cannot stand I'll fall on You
+Fadd9            G        C
+Jesus, You're my hope and stay
+
+[Chorus]
+        C         Fadd9  C    G
+Lord, I need You, oh,  I need You
+Am7   Fadd9  C    G
+Every hour I need You
+   C     Fadd9     C        Fadd9
+My one defense, my righteousness
+   C          G    C
+Oh God, how I need You
+
+// Repeat 2x`});
